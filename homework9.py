75c8b69a1209bd7f47a33cb8489ca96cfd220a9b
vakobabajanashvili <138526048+vakobabajanashvili@users.noreply.github.com>
2023-07-30 19:05:55 +0400

    Add files via upload

---
+++ homework9.py
@@ -0,0 +1,7 @@
+age = int(input("Enter Your Age: "))
+Birth_Year = int(input("Enter Your Birth Year: "))
+Finished_School = int(input("Enter Year When You Finished School: "))
+
+print("Not Allowed", age<18 )
+print("Under 18",  Birth_Year>2005 )
+print("Finished School", Finished_School<2023 )
