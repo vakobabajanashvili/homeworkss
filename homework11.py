4bd3f38921044adf16aa6acd44f0fc721ad4bdb5
vakobabajanashvili <138526048+vakobabajanashvili@users.noreply.github.com>
2023-08-15 22:29:55 +0400

    Add files via upload

---
+++ homework11.py
@@ -0,0 +1,19 @@
+workout_level = int(input("რამდენი დღე ვარჯიშობ კვირაში?: "))
+
+if workout_level < 1 or 2:
+    print("ცოტას ვარჯიშობ")
+
+elif workout_level < 3 or 4:
+    print("საშუალოდ ვარჯიშობ")
+
+else:
+    print("ბევრს ვარჯშობ")
+
+
+
+for i in range(3, 100, 3,):
+    print(str(i) + " " + 'GOA')
+for l in range(5, 100, 5):
+    print(str(l) + " " + 'GOA11')
+for k in range(15, 100, 15):
+    print(str(k) + " " + 'GOA15')
