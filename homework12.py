7e8a7848531c1840e5a2d64ada635b84ffc9ffa8
vakobabajanashvili <138526048+vakobabajanashvili@users.noreply.github.com>
2023-08-18 22:38:25 +0400

    Add files via upload

---
+++ homework12.py
@@ -0,0 +1,7 @@
+name = ["ბ", "ა", "ბ", "ა", "ჯ", "ა", "ნ", "ა", "შ", "ვ", "ი", "ლ", "ი"]
+print(name[1] + name[3] + name[5] + name[7] + name[10] + name[12])
+
+family = ['ვაკო', 'დათო', 'შორენა', "ბაბაჯანაშვილი", "გიორგაძე"]
+print(family[0] + " " + family[3])
+print(family[1] + " " + family[3])
+print(family[2] + " " + family[4])
