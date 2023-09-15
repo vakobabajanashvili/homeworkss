baeb6cff32b2e6b14bece9f64924c303c63e254d
vakobabajanashvili <138526048+vakobabajanashvili@users.noreply.github.com>
2023-09-15 23:28:03 +0400

    Add files via upload

---
+++ homework19.py
@@ -0,0 +1,14 @@
+janezas_razmi = ["alex rajavi", "anastasia feridze", "nika qatamadze", "nikoloz takidze", "nugzar turmanishvili", "roba amonashvili", "vako"]
+janezas_razmi2 = []
+my_name = "vefxia"
+my_name_lenght = len(my_name)
+
+janezas_razmi.pop(my_name_lenght)
+print(janezas_razmi)
+
+#----------------------------------------------------------------------------------------------------------------
+
+for i in range(my_name_lenght):
+    print("magaria")
+
+#----------------------------------------------------------------------------------------------------------------
